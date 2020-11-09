3c604de66bdca130cb39e4cc438522030f5eb423
Kim-jh2014 <thirdjong@g.skku.edu>
2020-11-09 19:21:50 +0900

    report card: math change (git commit --amend edit !!)

--- report_card.c
+++ report_card.c
@@ -3,7 +3,8 @@
 void main(){
 	printf("This is Report Cards.c\n");
 	printf("what is the difference ? \n");
-	printf("Math: 90 \n");
+	printf("Math: 0 \n");
 	printf("English: 100 \n");
 	printf("Sum grade: 190 \n");
+	printf("Average grade: 95 \n");
 }
